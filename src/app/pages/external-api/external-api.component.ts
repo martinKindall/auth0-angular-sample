beecb50972acbcae404b34d8a51908751b087bd1
martinKindall <mart256@gmail.com>
2021-03-07 22:03:27 +0100

    Showing description from order object

--- src/app/pages/external-api/external-api.component.ts
+++ src/app/pages/external-api/external-api.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment as env } from '../../../environments/environment';
+
+interface Message {
+  description: string;
+}
 
 @Component({
   selector: 'app-external-api',
   templateUrl: './external-api.component.html',
 })
 export class ExternalApiComponent implements OnInit {
-  message: string = null;
+  description: string = null;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {}
+
+  callSecureApi(): void {
+    this.http
+      .get(`${env.dev.serverUrl}/order-composite/10`)
+      .subscribe((result: Message) => {
+        this.description = result.description;
+      });
+  }
 }
